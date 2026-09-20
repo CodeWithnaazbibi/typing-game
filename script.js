e4d84c480ace3081daa7dc231ecbef273c7a4077
asleepjan <syedzwakmanjan@protonmail.com>
2026-09-20 22:41:16 +0000

    write important functions

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const gameTime = 30 * 1000;
 window.timer = null;
 window.gameStart = null;
 window.pauseTime = 0;
+
+function addClass(el,name) {
+  el.className += ' '+name;
+}
+function removeClass(el,name) {
+  el.className = el.className.replace(name,'');
+}
+
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordsCount);
+  return words[randomIndex - 1];
+}
+
+function formatWord(word) {
+  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+}
