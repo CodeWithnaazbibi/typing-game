1fff020e9fbde2c52b6f680757e633c190fe21bb
asleepjan <syedzwakmanjan@protonmail.com>
2026-09-20 22:45:44 +0000

    adding function to calculate words per minute typing speed

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ function newGame() {
   document.getElementById('info').innerHTML = (gameTime / 1000) + '';
   window.timer = null;
 }
+
+function getWpm() {
+  const words = [...document.querySelectorAll('.word')];
+  const lastTypedWord = document.querySelector('.word.current');
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
+  const typedWords = words.slice(0, lastTypedWordIndex);
+  const correctWords = typedWords.filter(word => {
+    const letters = [...word.children];
+    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+    const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+    return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+  });
+  return correctWords.length / gameTime * 60000;
+}
