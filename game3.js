4ffcc92fac2b0c9b740d8ac6b77d9da838df7965
asleepjan <syedzwakmanjan@protonmail.com>
2026-09-21 03:22:16 +0000

    add hero class which is an extention of game object class meaning has all those properties and inplemented functionality for movement, coin collection, and sprite animation functionality also add input class which handles the key presses and moves hero to that letter

--- game3.js
+++ game3.js
@@ -294,3 +294,63 @@ class GameObject {
     );
   }
 }
+
+class Hero extends GameObject {
+  constructor({ game, sprite, position, scale }) {
+    super({ game, sprite, position, scale });
+    this.speed = 100;
+    this.maxFrame = 2;
+  }
+
+  update(deltaTime) {
+    let nextX = this.destinationPosition.x;
+    let nextY = this.destinationPosition.y;
+
+    const scaledSpeed = this.speed * (deltaTime / 1000);
+
+    const distance = this.moveTowards(this.destinationPosition, scaledSpeed);
+
+    const arrived = distance <= scaledSpeed;
+
+    if (arrived) {
+      if (this.game.input.lastKey === "1") {
+        nextY -= TILE_SIZE;
+        this.sprite.y = 8;
+      } else if (this.game.input.lastKey === DOWN) {
+        nextY += TILE_SIZE;
+        this.sprite.y = 10;
+      } else if (this.game.input.lastKey === LEFT) {
+        nextX -= TILE_SIZE;
+        this.sprite.y = 9;
+      } else if (this.game.input.lastKey === RIGHT) {
+        nextX += TILE_SIZE;
+        this.sprite.y = 11;
+      }
+      const col = nextX / TILE_SIZE;
+      const row = nextY / TILE_SIZE;
+      if(this.game.world.getTile(this.game.world.level1.collisionLayer, row,col) !== 1){
+          this.destinationPosition.x = nextX;
+          this.destinationPosition.y = nextY;
+      }
+      if(this.game.world.getTile(this.game.world.level1.coinsLayer, row,col) !== 0){
+        coinsCollected += 1;
+        coinsCollectedElement.textContent = "Coins Collected: " + coinsCollected;
+        this.game.world.level1.coinsLayer[row * COLS + col] = 0;
+      } else if (coinsCollected == 149) {
+        coinsCollectedElement.textContent = "You have collected all 149 coins"
+      }
+    }
+
+    if (this.game.input.keys.length > 0 || !arrived) {
+      this.moving = true;
+    } else {
+      this.moving = false;
+    }
+
+    if (this.game.eventUpdate && this.moving) {
+      this.sprite.x < this.maxFrame ? this.sprite.x++ : (this.sprite.x = 0);
+    } else if (!this.moving) {
+      this.sprite.x = 0;
+    }
+  }
+}
